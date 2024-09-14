aae7f84f2083d5abe80f8a130bd0e5dc332d5f43
rashmiav24 <rashmiav249@gmail.com>
2024-09-14 18:44:13 +0530

    adding python file

---
+++ star_pattern.py
@@ -0,0 +1,19 @@
+#. Right-Angle Triangle (Stars)
+"""
+*
+**
+***
+****
+*****
+"""
+
+def pattern(n):
+    for i in range(1,n+1):
+        for j in range(1,i+1):
+            print('*',end="")
+        print()
+
+
+n = 5
+pattern(n)
+
